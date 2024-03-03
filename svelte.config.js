a21e197098dab1086d4a0b79c1f30ec6f4575644
Vedbhanushali <19ce005@charusat.edu.in>
2024-03-03 18:01:08 +0530

    deployment branch to main other branch is not allowing

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,7 @@ import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const dev = process.argv.includes('dev')
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
